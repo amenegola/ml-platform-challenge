986ea6167b2f38b6a0c86d4533961056277eaf7f
Afonso Menegola <afonsomenegola@gmail.com>
2021-04-04 19:33:24 -0300

    testando colocar policy dentro

--- terraform/modules/firehose/firehose.tf
+++ terraform/modules/firehose/firehose.tf
@@ -12,21 +12,39 @@ resource "aws_iam_role" "firehose_role" {
       },
       "Effect": "Allow",
       "Sid": ""
-    },
-    {
-            "Sid": "",
-            "Effect": "Allow",
-            "Action": [
-                "kinesis:DescribeStream",
-                "kinesis:GetShardIterator",
-                "kinesis:GetRecords",
-                "kinesis:ListShards"
-            ],
-            "Resource": "arn:aws:kinesis:us-east-2:741183806697:stream/random-beer-data-stream"
     }
   ]
 }
 EOF
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = "test-policy"
+  description = "A test policy"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Action": [
+          "kinesis:DescribeStream",
+          "kinesis:GetShardIterator",
+          "kinesis:GetRecords",
+          "kinesis:ListShards"
+      ],
+      "Resource": "arn:aws:kinesis:us-east-2:741183806697:stream/random-beer-data-stream"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "test-attach" {
+  role       = aws_iam_role.firehose_role.name
+  policy_arn = aws_iam_policy.policy.arn
 }
 
 resource "aws_s3_bucket" "bucket" {
