cac5808d9c939535de6f925a2bc2f76082b60fe1
Afonso Menegola <afonsomenegola@gmail.com>
2021-04-04 15:48:13 -0300

    Criando branch modules

--- terraform/envs/test/terraform.tf
+++ terraform/envs/test/terraform.tf
@@ -10,39 +10,20 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_kinesis_stream" "dev" {
-  name             = "random-beer-data-stream-${var.dev_prefix}"
-  shard_count      = 1
-  retention_period = 24
+module "kinesis_stream" {
+  source = "../../"
 
-  shard_level_metrics = [
-    "IncomingBytes",
-    "OutgoingBytes",
-  ]
-
-  tags = {
-    Environment = "development"
-  }
-}
-
-resource "aws_iam_role" "kinesis_role" {
-  name = "kinesis_role-${var.dev_prefix}"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "kinesis.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
+  name                      = var.name
+  shard_count               = var.shard_count
+  retention_period          = var.retention_period
+  shard_level_metrics       = var.shard_level_metrics
+  enforce_consumer_deletion = var.enforce_consumer_deletion
+  encryption_type           = var.encryption_type
+  kms_key_id                = var.kms_key_id
+  tags                      = var.tags
+  create_policy_read_only   = var.create_policy_read_only
+  create_policy_write_only  = var.create_policy_write_only
+  create_policy_admin       = var.create_policy_admin
 }
 
 resource "aws_s3_bucket" "raw_dev" {
