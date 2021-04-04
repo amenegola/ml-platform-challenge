b1b1323df3221ee6b523d5f5bcbc67e0fc8e2859
Afonso Menegola <afonsomenegola@gmail.com>
2021-04-04 17:44:00 -0300

    .

--- terraform/modules/firehose/firehose.tf
+++ terraform/modules/firehose/firehose.tf
@@ -1,3 +1,20 @@
+data "aws_iam_policy_document" "kinesis_firehose_stream_assume_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["firehose.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "kinesis_firehose_stream_role" {
+  name               = var.kinesis_firehose_stream_role_name
+  assume_role_policy = data.aws_iam_policy_document.kinesis_firehose_stream_assume_role.json
+}
+
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
   acl    = "private"
@@ -13,7 +30,7 @@ resource "aws_kinesis_firehose_delivery_stream" "firehose_stream" {
   }
   
   s3_configuration {
-    role_arn        = "${aws_iam_role.kinesis_firehose_stream_role.arn}"
+    role_arn        = aws_iam_role.kinesis_firehose_stream_role.arn
     bucket_arn      = aws_s3_bucket.bucket.arn
     buffer_size     = var.buffer_size
     buffer_interval = var.buffer_interval
